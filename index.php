2a1586e32a20bde9d641b9a279e30ba9da627e06
mentalaffect <parismay.frontend@mail.ru>
2022-09-01 13:31:12 +0300

    module3-task1

--- index.php
+++ index.php
@@ -9,9 +9,13 @@ use TaskForce\classes\actions\EndAction;
 
 require_once "vendor/autoload.php";
 
-$taskStatusNew = new Task(1, 1, 2);
-$taskUserCustomer = new Task(1, 1, 2);
-$taskUserPerformer = new Task(2, 1, 2);
+$task = null;
+
+try {
+    $task = new Task(Task::STATUS_UNDEFINED, 1, 1);
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
 
 $createAction = new CreateAction();
 $cancelAction = new CancelAction();
@@ -19,36 +23,13 @@ $acceptAction = new AcceptAction();
 $declineAction = new DeclineAction();
 $endAction = new EndAction();
 
-// Установка настроек тестирования
-assert_options(ASSERT_ACTIVE, true);
-assert_options(ASSERT_WARNING, false);
-assert_options(ASSERT_BAIL, false);
-assert_options(ASSERT_EXCEPTION,  false);
-assert_options(ASSERT_CALLBACK, function($file, $line, $assertion, $message) {
-    echo "$message <br>";
-});
-
 // Проверка каждого действия на следующий за ним статус задания
-assert($taskStatusNew->getNextStatus($createAction) === Task::STATUS_NEW, 'Ошибка статуса "Новое задание" (STATUS_NEW)');
-assert($taskStatusNew->getNextStatus($cancelAction) === Task::STATUS_CANCELED, 'Ошибка статуса "Задание отменено" (STATUS_CANCELED)');
-assert($taskStatusNew->getNextStatus($acceptAction) === Task::STATUS_IN_WORK, 'Ошибка статуса "Задание отменено" (STATUS_IN_WORK)');
-assert($taskStatusNew->getNextStatus($declineAction) === Task::STATUS_FAILED, 'Ошибка статуса "Задание отменено" (STATUS_FAILED)');
-assert($taskStatusNew->getNextStatus($endAction) === Task::STATUS_PERFORMED, 'Ошибка статуса "Задание отменено" (STATUS_PERFORMED)');
-
-// Проверка доступных (CREATE_ACTION) пользователю, являющемуся заказчиком, действий для нового (STATUS_NEW) задания
-assert($taskUserCustomer->getAvailableActions() === [$createAction::class], 'Ошибка действия "Создать задание" (CreateAction)');
-
-// Установка заданию, в котором пользователь является заказчиком, статуса "Новое задание" (STATUS_NEW)
-$taskUserCustomer->status = Task::STATUS_NEW;
-// Проверка доступных (CANCEL_ACTION) пользователю, являющемуся заказчиком, действий для нового (STATUS_NEW) задания
-assert($taskUserCustomer->getAvailableActions() === [$cancelAction::class], 'Ошибка "Отменить задание" (CancelAction)');
-
-// Установка заданию, в котором пользователь является исполнителем, статуса "Новое задание" (STATUS_NEW)
-$taskUserPerformer->status = Task::STATUS_NEW;
-// Проверка доступных (ACCEPT_ACTION) пользователю, являющемуся исполнителем, действий для нового (STATUS_NEW) задания
-assert($taskUserPerformer->getAvailableActions() === [$acceptAction::class], 'Ошибка действия "Принять задание" (AcceptAction)');
-
-// Установка заданию, в котором пользователь является исполнителем, статуса "На исполнении" (STATUS_IN_WORK)
-$taskUserPerformer->status = Task::STATUS_IN_WORK;
-// Проверка доступных (DECLINE_ACTION, END_ACTION) пользователю, являющемуся исполнителем, действий для принятого (STATUS_IN_WORK) задания
-assert($taskUserPerformer->getAvailableActions() === [$declineAction::class, $endAction::class], 'Ошибка действия "Отказ от задания" (DeclineAction)');
+try {
+    echo $task->getNextStatus($createAction) . '<br>';
+    echo $task->getNextStatus($cancelAction) . '<br>';
+    echo $task->getNextStatus($acceptAction) . '<br>';
+    echo $task->getNextStatus($declineAction) . '<br>';
+    echo $task->getNextStatus($endAction) . '<br>';
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
--- src/classes/entity/Task.php
+++ src/classes/entity/Task.php
@@ -8,6 +8,8 @@ use TaskForce\classes\actions\CancelAction;
 use TaskForce\classes\actions\AcceptAction;
 use TaskForce\classes\actions\DeclineAction;
 use TaskForce\classes\actions\EndAction;
+use TaskForce\classes\exceptions\WrongStatusException;
+use TaskForce\classes\exceptions\WrongActionException;
 
 class Task
 {
@@ -18,7 +20,8 @@ class Task
     const STATUS_PERFORMED = 'performed';
     const STATUS_FAILED = 'failed';
 
-    const TASK_MAP = [
+    const STATUS_MAP = [
+        self::STATUS_UNDEFINED => 'Не создано',
         self::STATUS_NEW => 'Новое',
         self::STATUS_CANCELED => 'Отменено',
         self::STATUS_IN_WORK => 'На исполнении',
@@ -45,13 +48,32 @@ class Task
     private int $customerId;
     private int $performerId;
 
-    public function __construct(int $userId, int $customerId, int $performerId)
+    /**
+     * @param int $userId
+     * @param int $customerId
+     * @param int $performerId
+     * @param string $status
+     * @throws WrongStatusException
+     */
+    public function __construct(string $status, int $userId, int $customerId, int $performerId = 0)
     {
         $this->userId = $userId;
         $this->customerId = $customerId;
         $this->performerId = $performerId;
+
+        if (!self::STATUS_MAP[$status]) {
+            throw new WrongStatusException("Передано несуществующее имя статуса <br>");
+        }
+
+        $this->status = $status;
     }
 
+    /**
+     * Переданное действие возвращает статус, который получит задание при его выполнении.
+     * @param AbstractAction $AbstractAction
+     * @return string
+     * @throws WrongActionException
+     */
     public function getNextStatus(AbstractAction $AbstractAction): string
     {
         return match ($AbstractAction::class) {
@@ -60,10 +82,14 @@ class Task
             AcceptAction::class => self::STATUS_IN_WORK,
             DeclineAction::class => self::STATUS_FAILED,
             EndAction::class => self::STATUS_PERFORMED,
-            default => '',
+            default => throw new WrongActionException('Передано несуществующее действие'),
         };
     }
 
+    /**
+     * Возвращает список возможных действий для текущего статуса задания, в зависимости от прав пользователя.
+     * @return array
+     */
     public function getAvailableActions(): array
     {
         $availableActionsList = self::ACTIONS_MAP[$this->status] ?? [];
